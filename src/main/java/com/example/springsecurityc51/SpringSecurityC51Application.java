d488b36abebc7856c8cc6f852e1ba4ad1f10739f
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-04-10 21:36:00 +0300

    Initial commit

---
+++ src/main/java/com/example/springsecurityc51/SpringSecurityC51Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityC51Application.class, args);
+    }
+
+}
